285a2c361115505ef58aba03dde164bacbb8f06a
atomsk <atomskjd@gmail.com>
2024-05-25 00:02:33 +0500

    v1.0.0 release

---
+++ cmd/api/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0" // just in case I don't generate it et
+
+// config hold all configuration settings
+type config struct {
+	port int
+	env  string
+}
+
+// application hold the dependencies for HTTP handlers, helpers, middleware
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment type (development|staging|production)")
+	flag.Parse()
+
+	//init new logger
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// create App instance
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// routes
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+}
