069e29d88f68df7228b3b3a23361593d5eb9d14d
atomsk <atomskjd@gmail.com>
2024-05-31 13:30:25 +0500

    ❗️ finish with error handling

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -45,7 +46,15 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
-	err := json.NewDecoder(r.Body).Decode(dst)
+	// 1: ограничим размер тела запроса
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	// 2: запрещаем лишние поля
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
@@ -69,13 +78,25 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 		case errors.Is(err, io.EOF):
 			return errors.New("тело запроса не должно быть пустым")
 
-			// TODO panicking vs erroring is discussable
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("тело запроса содержит неизвестное поле %s", fieldName)
+
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("тело запроса должно быть меньше %d байт", maxBytes)
+			// panicking vs erroring is discussable
+
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
 
 		default:
 			return err
 		}
+
+	}
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("тело запроса должно содержать только 1 JSON")
 
 	}
 
