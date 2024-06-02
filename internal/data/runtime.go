e40d7f8572eda055aa811075e70fb216ce600d05
atomsk <atomskjd@gmail.com>
2024-06-02 20:27:21 +0500

    Decode input json

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("не корректный формат runtime")
 
 type Runtime int32
 
@@ -15,4 +19,27 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 
 	// convert string to byte array
 	return []byte(quotedJSONValue), nil
+}
+
+// UnmarshalJSON implements Unmarshaler interface
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "мин." {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
