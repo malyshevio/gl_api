41959f73d6610c9b4304c51c7b4fccf75fc2779c
atomsk <atomskjd@gmail.com>
2024-06-26 21:19:29 +0500

    insert query

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"time"
 
+	"github.com/lib/pq"
 	"gl_api.malyshev.io/internal/validator"
 )
 
@@ -41,7 +42,15 @@ type MovieModel struct {
 
 // Insert method to movie DB
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+	query := `
+		INSERT INTO movies (title, year, runtime, genres)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version
+	`
+
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 // Get method from movie DB
