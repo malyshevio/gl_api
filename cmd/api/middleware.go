980d526e538adec2e4031326684050865775b381
atomsk <atomskjd@gmail.com>
2024-08-12 12:37:53 +0500

    requiring activation add 1.5 middleware helper

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -134,17 +134,29 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 	})
 }
 
+// run 2 middleware
 func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
+		if !user.Activated {
+			app.inactiveAccountResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+
+	// run second
+	return app.requireAuthenticatedUser(fn)
+}
+
+func (app *application) requireAuthenticatedUser(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user := app.contextGetUser(r)
 
 		if user.IsAnonymous() {
 			app.authenticationRequiredResponse(w, r)
-			return
-		}
-
-		if !user.Activated {
-			app.inactiveAccountResponse(w, r)
 			return
 		}
 
